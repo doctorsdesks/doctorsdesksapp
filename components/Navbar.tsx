816c5cba2b2a3938c17793977ef1c75d36ad3f2f
70070228 <sumit.kumar02@piramal.com>
2025-03-23 23:44:41 +0530

    appointment with both themes done

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -24,7 +24,7 @@ const Navbar: React.FC<NavbarProps> = ({ data, onClick }) => {
                         key={item?.label}
                         id={item?.label} 
                         onPress={() => onClick(item?.label)}
-                        style={{ width: (width-32)/data?.length, display: 'flex', alignItems: 'center', borderBottomColor: item?.isActive ? "#2DB9B0" : "#A9A9AB", borderBottomWidth: item?.isActive ? 2 : 1, paddingVertical: 12, paddingHorizontal: 16 }} 
+                        style={{ width: (width-32)/data?.length, display: 'flex', alignItems: 'center', borderBottomColor: item?.isActive ? "#2DB9B0" : "#fff", borderBottomWidth: item?.isActive ? 2 : 0, paddingVertical: 12, paddingHorizontal: 16, paddingBottom: item?.isActive ? 12 : 14 }} 
                     >
                         <CustomText multiLingual={true} textStyle={{ fontSize: 14, lineHeight: 20, fontWeight: item?.isActive ? 700 : 600, color: item?.isActive ? "#2DB9B0" : "#A9A9AB"}} text={item?.label} />
                     </Pressable>
